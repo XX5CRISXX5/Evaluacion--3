0c42a6b49ce58d3fcd8776271a149319789068e8
XX5CRISXX5 <80927246+XX5CRISXX5@users.noreply.github.com>
2021-04-13 15:44:13 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,19 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAbY0-FWSbs_vdCwPpyYmzFD26zXT-1-MA",
+  authDomain: "evaluacion-c8d54.firebaseapp.com",
+  databaseURL: "https://evaluacion-c8d54-default-rtdb.firebaseio.com",
+  projectId: "evaluacion-c8d54",
+  storageBucket: "evaluacion-c8d54.appspot.com",
+  messagingSenderId: "233093131236",
+  appId: "1:233093131236:web:68c84c7283fa4ad627f5f1",
+  measurementId: "G-3LRS8QXF0B"
+});
